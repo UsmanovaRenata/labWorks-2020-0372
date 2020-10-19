7bbe0c4b017bb6a5da767f9a856a07395d8dd3aa
Kalk <elina.paxar@gmail.com>
2020-10-20 02:31:49 +0300

    Created Lab_1

---
+++ PaxarevskayaES/lab_1/PR2.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <iostream>
+#include <conio.h>
+char ch;
+using namespace std;
+int main() {
+	float a1;
+	float a2;
+	float res;
+	char sign;
+	do {
+		cout << "Enter the first number" << endl;
+		while (!(cin >> a1)) {
+			cout << "Wrong type" << endl;
+			cin.clear();
+			cin.ignore();
+		}
+		cout << "Enter the second number" << endl;
+		while (!(cin >> a2)) {
+			cout << "Wrong type" << endl;
+			cin.clear();
+			cin.ignore();
+		}
+		cout << "Enter sign" << endl;
+		while (!(cin >> sign)) {
+			cout << "Wrong type" << endl;
+			cin.clear();
+			cin.ignore();
+		}
+		if (sign == '+') {
+			res = a1 + a2;
+			cout << res << endl;
+		}
+		if (sign == '-') {
+			res = a1 - a2;
+			cout << res << endl;
+		}
+		if (sign == '*') {
+			res = a1 * a2;
+			cout << res << endl;
+		}
+		if (sign == '/') {
+			if (a2 != 0) {
+				res = a1 / a2;
+				cout << res << endl;
+			}
+			else {
+				cout << "Action is impossible" << endl;
+			}
+		}
+		cout << "Press esc to exit or any other key to continue" << endl;
+		ch = _getch();
+	} while (ch != 27);
+}
