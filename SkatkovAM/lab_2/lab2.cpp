7f0eec034ad0b9a2da9efd194a3e6eb2b1271f13
craftmaster1231 <32911478+craftmaster1231@users.noreply.github.com>
2020-10-27 14:49:37 +0300

    Update lab2.cpp

--- SkatkovAM/lab_2/lab2.cpp
+++ SkatkovAM/lab_2/lab2.cpp
@@ -1,62 +1,82 @@
 
 #include <iostream>
 
-#include <ctime>
-using namespace std;
+    #include <ctime>
+    using namespace std;
 int main() {
-    const int basicSize=10;
-    const int newSize= basicSize / 2;
-int arr[basicSize];
-    srand(static_cast<int>(time(0)));
+    const int maxValueForRand= 10;
+    const int size=10;
+    int arr[size];
+    int arr2[size];
+    srand(time(0));
     cout<<"Basic array: ";
-for(int i=0; i < basicSize; i++){
-    arr[i]=rand()%10+1;
-    cout<<arr[i]<<' ';
-}
-    int i=0;
-int im_helpful;
-while (i < basicSize - 1){
-    int j=0;
-    while(j < basicSize - 1) {
-        if (arr[j] >arr[j + 1]) {
-            im_helpful=arr[j+1];
-            arr[j+1]=arr[j];
-            arr[j]=im_helpful;
-        }
-        j++;
-    }
-    i++;
-}
+    {
+        int j = 0;
+        for (int i = 0; i < size; i++) {
+            arr[i] = rand() % maxValueForRand + 1;
+            cout << arr[i] << ' ';
+            if (arr[i] % 2) {
+                arr2[j] = arr[i];
+                j++;
 
-
-
-
-cout<<endl<<"Sorted: ";
-    for(int i=0; i < basicSize; i++){
-        cout<<arr[i]<<' ';
-    }
-
-int arr2[newSize];
-
-    for(int i=0;i<newSize;i++){
-    arr2[i]=arr[2*i+1];
-
-    }
-    // int max=arr2[0],min=arr2[0];
-    int summ=0;
-    cout<<endl<<"New array: ";
-    for(int i=0;i<newSize;i++){
-        cout<<arr2[i]<<' ';
-        /*  if (arr2[i]>max){
-            max=arr2[i];
+            }else{
+                arr2[j]= maxValueForRand+1; // unreal value for arr[i]  to check later
             }
-            if (arr2[i]<min){
-            min=arr2[i];
-            } */
-        summ=summ+arr2[i];
+        }
     }
-cout<<endl<<"Min: "<<arr2[0]<<endl<<"Max: "<< arr2[newSize-1]<< endl<< "Mid: "<< float(summ)/newSize;
-    return 0;
 
+    {
+        int i = 0;
+        int im_helpful;
+        while (i < size - 1) {
+            int j = 0;
+            while (j < size - 1) {
+                if (arr[j] > arr[j + 1]) {
+                    im_helpful = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = im_helpful;
+                }
+                j++;
+            }
+            i++;
+        }
+    }
+
+
+
+        cout<<endl<<"Sorted: ";
+        for(int i=0; i < size; i++){
+            cout<<arr[i]<<' ';
+        }
+        cout<<endl<<"New: ";
+        int summ=0,max=arr2[0],min=arr2[0];
+        int i=0;
+        for( i;i<size;i++){
+            if(arr2[i]%2 && arr2[i]!=maxValueForRand+1) {
+                cout << arr2[i] << " ";
+                summ += arr2[i];
+                if(arr2[i]>max){
+                    max=arr2[i];
+                }
+                if (arr2[i]<min){
+                    min=arr2[i];
+                }
+            }else{
+                break;
+            }
+
+        }
+        cout<<endl<<"Min: "<<min<<endl<<"Max: "<< max<< endl<< "Mid: "<< float(summ)/(i-1);
+        return 0;
 
 }
+
+
+
+
+
+
+
+
+
+
