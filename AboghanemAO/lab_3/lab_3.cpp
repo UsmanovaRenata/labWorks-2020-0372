7e0341e424bba27873b8654cde1538b7868af54b
aboghanem <abo_ghanem.awsanPS@yahoo.com>
2020-11-28 22:59:59 +0300

    fixed

--- AboghanemAO/lab_3/lab_3.cpp
+++ AboghanemAO/lab_3/lab_3.cpp
@@ -1,61 +1,57 @@
 #include <iostream>
 
+int Sum(char* word) {
+	int sum = 0;
+	for (int i = 0; i < (int)strlen(word); i++) {
+		sum = sum + int(word[i]);
+	}
+	return sum;
+}
 
 
-void Sort(char** words, int words_counter) {
-	char* test = words[0];
+void Sort(char words[25][15], int words_counter) {
+	char temp[15];
 	for (int i = 0; i < words_counter; i++) {
 		for (int j = i + 1; j < words_counter; j++) {
-			test = words[i];
-			if ((int)strlen(test) > (int)strlen(words[j])) {
-				words[i] = words[j];
-				words[j] = test;
+			strcpy_s(temp, 15, words[i]);
+			if (Sum(temp) > Sum(words[j])) {
+				strcpy_s(words[i], 15, words[j]);
+				strcpy_s(words[j], 15, temp);
 			}
 		}
 	}
 }
 
+
+
 int main() {
-	char sen[100];
+	char sentence[101];
+	char split[25][15];
 
 
-	std::cout << "Enter your sentence : ";
-	std::cin.getline(sen, 100);
+	std::cout << "Enter your sentence : (max 100 symbols and 25 words) ";
+	std::cin.getline(sentence, 101);
 
 
-	int words_counter = 1;
-	for (int i = 0; i < (int)strlen(sen); i++) {
-		if (sen[i] == ' ') {
-			words_counter++;
-		}
-	}
 
-	int index = 0;
-	//Create char array of char arrays to store splitted string
-	char** words = new char* [words_counter];
-	for (int i = 0; i < words_counter; i++) {
-		words[index++] = new char[100];
-	}
-
-
-	int word = 0;
-	int letter = 0;
-	for (int i = 0; i <= (int)strlen(sen); i++) {
-		if (sen[i] == ' ') {
-			words[word][letter] = '\0';
-			word++;
-			letter = 0;
+	int indx_word = 0, indx_letter = 0;
+	for (int i = 0; i < (int)strlen(sentence) + 1; i++) {
+		if (sentence[i] != ' ' && sentence[i] != '\0') {
+			split[indx_word][indx_letter++] = sentence[i];
 		}
 		else {
-			words[word][letter++] = sen[i];
+			split[indx_word][indx_letter] = '\0';
+			indx_word++;
+			indx_letter = 0;
 		}
 	}
 
 
-	Sort(words, words_counter);
+
+	Sort(split, indx_word);
 	std::cout << "Result : ";
-	for (int i = 0; i < words_counter; i++) {
-		std::cout << words[i] << " ";
+	for (int i = 0; i < indx_word; i++) {
+		std::cout << split[i] << " ";
 	}
 	std::cout << std::endl;
 
