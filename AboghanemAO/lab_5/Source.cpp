06acf39478cf467bf5b6ed108c26a1bc7ef5ba69
aboghanem <abo_ghanem.awsanPS@yahoo.com>
2020-11-28 23:51:32 +0300

    fixed

--- AboghanemAO/lab_5/Source.cpp
+++ AboghanemAO/lab_5/Source.cpp
@@ -1,69 +1,122 @@
 #include <iostream>
-#include <time.h>
+#include <conio.h>
+using namespace std;
+const int rows = 2, columns = 3;
+
+
+
+
+void print_answer(int answer[rows][columns]) {
+	std::cout << "Result :" << std::endl;
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+			std::cout << answer[i][j] << "  ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+void Multiplication(int matrix_1[rows][columns], int matrix_2[rows][columns], int answer[rows][columns]) {
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+			answer[i][j] = 0;
+			for (int z = 0; z < rows; z++) {
+				answer[i][j] += matrix_1[i][z] * matrix_2[z][j];
+			}
+		}
+	}
+	print_answer(answer);
+}
+
+
+
+void sum(int matrix_1[rows][columns], int matrix_2[rows][columns], int answer[rows][columns]){
+	
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+			answer[i][j] = matrix_1[i][j] + matrix_2[i][j];
+		}
+	}
+	print_answer(answer);
+	
+}
+
+void Subtraction(int matrix_1[rows][columns], int matrix_2[rows][columns], int answer[rows][columns]) {
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+			answer[i][j] = matrix_1[i][j] - matrix_2[i][j];
+		}
+	}
+	print_answer(answer);
+	
+}
+
+
 
 
 int main() {
-	srand(time(NULL));
-	
-	
-	int arr1[5];
-	int arr2[5];
+	int matrix_1[rows][columns];
+	int matrix_2[rows][columns];
+	int answer[rows][columns];
 	int operation;
-	bool wrong_input = false;
+	char end_pro;
 
-	std::cout << "enter first arr : ";
-	for (int i = 0; i < 5 ; i++) {
-		std::cin >> arr1[i];
-	
-	}
-	std::cout << "enter second arr : ";
-	for (int i = 0; i < 5; i++) {
-		std::cin >> arr2[i];
+	bool right_input = true;
 
-	}
+
 
 
 	do {
-		std::cout << "choice what operation to carry out :\n1) Multiplication\n2) Addition\n3) Subtraction \n";
-		std::cin >> operation;
-
-		switch (operation)
-		{
-		case 1:
-			std::cout << "Multiplication : ";
-			for (int i = 0; i < 5; i++) {
-				std::cout << arr1[i] * arr2[i] << ' ';
+		std::cout << "Enter first matrix : \n";
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				std::cout << "Enter the value of matrix_1" << i + 1 << "][" << j + 1 << "] : ";
+				std::cin >> matrix_1[i][j];
 			}
-			break;
-
-		case 2:
-			std::cout << "Addition : ";
-			for (int i = 0; i < 5; i++) {
-				std::cout << arr1[i] + arr2[i] << ' ';
+		}
+		std::cout << "enter second matrix : \n";
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				std::cout << "Enter the value of matrix_2[" << i + 1 << "][" << j + 1 << "] : ";
+				std::cin >> matrix_2[i][j];
 			}
-			break;
-
-		case 3:
-			std::cout << "Subtraction : ";
-			for (int i = 0; i < 5; i++) {
-				std::cout << arr1[i] - arr2[i] << ' ';
-			}
-			break;
-
-		default:
-			wrong_input = true;
-			std::cout << "wrong input operation try agin";
-			break;
 		}
 
-	} while (wrong_input);
-	
 
-	
+		std::cout << "Choice what operation to carry out :\n1) Multiplication\n2) Addition\n3) Subtraction \n";
+		std::cin >> operation;
 
 
+		do {
+			switch (operation)
+			{
+			case 1:
+				Multiplication(matrix_1, matrix_2, answer);
+				break;
+
+			case 2:
+				sum(matrix_1, matrix_2, answer);
+				break;
+
+			case 3:
+				Subtraction(matrix_1, matrix_2, answer);
+				break;
+
+			default:
+				std::cout << "wrong input operation try agin";
+				right_input = false;
+				break;
+			}
+		} while (!right_input);
+		
+
+
+		std::cout << "press ESC to close the program or anything else to continue : ";
+		end_pro = _getch();
+		std::cout << "\n";
+	} while (end_pro != 27);
 
 	std::cout << "\n";
-	system("pause");
 	return 0;
 }
