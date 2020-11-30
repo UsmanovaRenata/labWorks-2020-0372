87f6cdda621f47cdf9a1280cbd7477b4667ddd17
craftmaster1231 <32911478+craftmaster1231@users.noreply.github.com>
2020-11-30 11:47:54 +0300

    Update lab2.cpp

--- SkatkovAM/lab_2/lab2.cpp
+++ SkatkovAM/lab_2/lab2.cpp
@@ -1,6 +1,4 @@
-
 #include <iostream>
-
 #include <ctime>
 using namespace std;
 void basicArrayInit(int * arr, int size, int max){
@@ -8,19 +6,19 @@ void basicArrayInit(int * arr, int size, int max){
         arr[i] = rand() % max+ 1;
     }
 }
-void oddArrayInit(const int * basic_arr,int *odd_arr, const int size, int max){
+
+
+void oddArrayInit(const int * basic_arr,int *odd_arr, const int basic_size, int & odd_size){
     int odd_arr_ind=0;
-    odd_arr[odd_arr_ind]=max+1;//unreal value for basic_array[i]  to check later
-    for(int i=0;i<size;i++){
-        odd_arr[i+1 - (i==size-1)]=max+1; // weird index to not to init odd_arr[size]
-        if (basic_arr[i]%2){
-        odd_arr[odd_arr_ind]=basic_arr[i];
+    for(int i = 0; i<basic_size; i++){
+        if(basic_arr[i]%2){
+            odd_arr[odd_arr_ind]=basic_arr[i];
             odd_arr_ind++;
-
         }
-
     }
+    odd_size=odd_arr_ind;
 }
+
 
 void sortAnyArray(int * arr,const int size){  //bubbly
     int im_helpful;
@@ -34,76 +32,59 @@ void sortAnyArray(int * arr,const int size){  //bubbly
         }
     }
 }
-int sumOfAnyArray(const int *arr,  const int size, const int max){
+
+
+int sumOfAnyArray(const int *arr,  const int size){
     int sum=0;
     for (int i = 0; i < size;i++){
-        if (arr[i] != max+1){
             sum+=arr[i];
-        }
     }
     return sum;
 }
-int trueLengthOfAnyArray(const int *arr,const  int size,const int max){
-    int length=0;
-    for(int i=0;i<size;i++){
-        if(arr[i] != max+1){ // != max+1 because array can be odd 
-            length++;
-        }
-    }
-    return length;
-}
 
 
-void printAnyArray( const int * arr,const int size, const int max) {
+void printAnyArray( const int * arr,const int size) {
     for (int i = 0; i < size; i++) {
-        if (arr[i] != max + 1) {
             cout << arr[i] << ' ';
-        }
+
     }
     cout << endl;
 }
-int minValueOfAnyArray(const int * arr, const int size, const int max_allowed_value_for_each_element ){
+
+
+int minValueOfAnyArray(const int * arr, const int size ){
     int min_value_of_array = arr[0];
     for(int i = 0; i<size;i++){
-        if(arr[i]<min_value_of_array && arr[i] != max_allowed_value_for_each_element+1){
+        if(arr[i]<min_value_of_array){
             min_value_of_array=arr[i];
         }
     }
     return  min_value_of_array;
 }
+
+
 int main() {
     const int maxArrValue= 10;
     const int size=10;
-
     srand(time(0));
-
     int basic_arr[size];
-
-    basicArrayInit(basic_arr,size, maxArrValue);
+    basicArrayInit(basic_arr,size,maxArrValue);
     cout<<"Basic array:"<<endl;
-    printAnyArray(basic_arr,size, maxArrValue);
-
+    printAnyArray(basic_arr,size);
     sortAnyArray(basic_arr,size);
-
     cout<<"Sorted basic array:"<<endl;
-    printAnyArray(basic_arr,size, maxArrValue);
-
+    printAnyArray(basic_arr,size);
     int odd_arr[size];
-
-    oddArrayInit(basic_arr,odd_arr,size,maxArrValue);
-
+    int odd_arr_size=0;
+    oddArrayInit(basic_arr,odd_arr,size,odd_arr_size);
     cout<<"Odd array:"<<endl;
-    if ( odd_arr[0] == maxArrValue+1){  // If no odd elements in basic array
+    if ( odd_arr_size==0){  // If no odd elements in basic array
         cout<<"Nothing here :)"<<endl;
         return 1;
     }
-        printAnyArray(odd_arr,size,maxArrValue);
-    cout<<"Mid value:\t"<<float(sumOfAnyArray(odd_arr,size,maxArrValue))/trueLengthOfAnyArray(odd_arr,size,maxArrValue)<<endl;
+    printAnyArray(odd_arr,odd_arr_size);
+    cout<<"Mid value:\t"<<float(sumOfAnyArray(odd_arr,odd_arr_size))/odd_arr_size<<endl;
     cout<<"Max value: \t"<<odd_arr[0]<<endl;
-    cout<<"Min value: \t"<< minValueOfAnyArray(odd_arr,size, maxArrValue)<<endl;
-
+    cout<<"Min value: \t"<< minValueOfAnyArray(odd_arr,odd_arr_size)<<endl;
     return 0;
-
-
 }
-
